801c41821447b26ea57d9707c50fbc3c29d66e49
sandyfromnyc <sandyfromnyc@gmail.com>
2023-02-06 23:04:43 -0500

    added show file

--- controllers/flights.js
+++ controllers/flights.js
@@ -4,24 +4,29 @@ const Flight = require("../models/flight");
 module.exports = {
   index,
   new: newFlight,
-  create
-};
-
-
-//function show(req, res) {
- // Flight.findById(req.params.id, function(err, flight) {
-  //  if(err) return res
- // })
-
-
-//}
-
-
-function index(req, res) {
-  Flight.find({}, function (err, flights) {
-    res.render("flights/index", { flights, title: 'Flights' });
-  });
+  create,
+  show
 }
+
+
+function show(req, res) {
+  Flight.findById(req.params.id, function(err, flight) {
+    if(err) return res.render("flights/index")
+    res.render("flights/show", {flight, title: "Flight Details"})
+  })
+}
+
+function create(req, res) {
+  for(let key in req.body) {
+    if(req.body[key] === '') delete req.body[key]
+  };
+  Flight.create(req.body, (err) => {
+    if(err) return res.render('flights/new')
+    res.redirect('/flights')
+  })
+}
+
+
 function newFlight(req, res) {
   const newFlight = new Flight();
 // Obtain the default date
@@ -32,8 +37,9 @@ departsDate += `-${dt.getDate().toString().padStart(2, '0')}T${dt.toTimeString()
 res.render('flights/new', { departsDate, title: "New Flight" });
 }
 
-function create(req, res) {
-  Flight.create(req.body, function(err, flight) {
-      res.redirect('/flights');
-  });
+function index(req, res) {
+  Flight.find({}, function(err, flights){
+    flights.sort((first, second) => first.departs - second.departs);
+    res.render('flights/index', {flights})
+  })
 }
--- models/flight.js
+++ models/flight.js
@@ -2,6 +2,16 @@ const mongoose = require('mongoose');
 // Shortcut to the mongoose.Schema class
 const Schema = mongoose.Schema;
 
+const destinationSchema = new Schema ({
+    airport: {
+      type: String,
+      enum: ['LAX', 'SFQ', 'FLL', 'MIA', 'JFK', 'LGA', 'EWR', 'DEN']
+    },
+    arrival: {
+      type: Date,
+    },
+  })
+
 const flightSchema = new Schema({
     airline: {
         type: String,
@@ -25,7 +35,8 @@ const flightSchema = new Schema({
         let today = new Date();
         return today.setFullYear(today.getFullYear() + 1)
         }
-    }
+    },
+    destinations: [destinationSchema],
 })
 
 module.exports = mongoose.model('Flight', flightSchema);
