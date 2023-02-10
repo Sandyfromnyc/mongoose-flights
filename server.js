4b7057f27a47b82f2fe6a2986067237afc36d66a
sandyfromnyc <sandyfromnyc@gmail.com>
2023-02-10 15:11:16 -0500

    added method override

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
+
 require('dotenv').config();
 require('./config/database');
 
@@ -24,6 +25,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(methodOverride('_method')); 
 
 app.use('/', indexRouter);
 app.use('/flights', flightsRouter);
